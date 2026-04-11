9ae17fa3f7f28d9a847ef7d05aa61715fae3f744
yash <yashgarg2810@gmail.com>
2026-04-11 13:29:05 +0530

    update API URL to production

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://my-auth-app-production-e784.up.railway.app'
 })
 
 export const registerUser = (data: { name: string; email: string; password: string }) =>
